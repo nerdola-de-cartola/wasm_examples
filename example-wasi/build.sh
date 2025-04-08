10df7a2571799f86f415aa04061fbe243302d011
Matheus Lucas <nerdola.cartola@gmail.com>
2025-04-08 17:52:35 -0300

    Update build script

--- example-wasi/build.sh
+++ example-wasi/build.sh
@@ -2,5 +2,5 @@ cd wasm-lib/
 cargo build
 cd ..
 cp target/wasm32-wasip1/debug/wasm_lib.wasm wasm_lib.wasm
-wasm-tools print wasm_lib.wasm -o wams-lib.wat
+wasm-tools print wasm_lib.wasm -o wams_lib.wat
 cargo run
